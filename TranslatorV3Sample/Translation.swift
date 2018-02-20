91145c6a91347617605b0682abc7da85b5d76b37
KellyAltom <kellyaltom@hotmail.com>
2018-02-20 09:56:42 -0800

    test

--- TranslatorV3Sample/Translation.swift
+++ TranslatorV3Sample/Translation.swift
@@ -30,6 +30,7 @@ class Translation: UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("test")
     }
     
     override func didReceiveMemoryWarning() {
