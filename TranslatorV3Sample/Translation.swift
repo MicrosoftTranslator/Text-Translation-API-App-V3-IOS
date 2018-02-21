e1dc26cd8a4c118ece9440a5d986ff6e3ae576de
KellyAltom <kellyaltom@hotmail.com>
2018-02-21 15:43:02 -0800

    added code to request translation and parse son

--- TranslatorV3Sample/Translation.swift
+++ TranslatorV3Sample/Translation.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class Translation: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    
+    
     //*****used after parsing need to put into an array of structs
     struct AllLangDetails: Codable {
         var code = String()
@@ -31,6 +33,7 @@ class Translation: UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
     @IBOutlet weak var fromLangPicker: UIPickerView!
     @IBOutlet weak var toLangPicker: UIPickerView!
     @IBOutlet weak var textToTranslate: UITextView!
+    @IBOutlet weak var translatedText: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,32 +57,85 @@ class Translation: UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
     
     @IBAction func getTranslationBtn(_ sender: Any) {
         
-        let azureKey = "18358F19A2E74F80-A7C5BE039C8E614D"
-        let contentType = "Content-Type/json"
-        let traceID = "a1d2e356-ded0-8cce-c5a11d34589"
+        struct encodeText: Codable {
+            var text = String()
+        }
+        
+        let azureKey = "31b6016565ac4e1585b1fdb688e42c6d"
+        let contentType = "application/json"
+        let traceID = "A14C9DB9-0DED-48D7-8BBE-C517A1A8DBB0"
         let host = "dev.microsofttranslator.com"
-        let sampleLangAddress = "https://dev.microsofttranslator.com/translate?api-version=3.0&from=en&to=de"
+        let apiURL = "https://dev.microsofttranslator.com/translate?api-version=3.0&from=en&to=de"
         
-        let textToTranslate = TranslatedStrings(text: "my dog is very happy", to: "de")
-        let jsonToTranslate = try? jsonEncoder.encode(textToTranslate.self)
+        let text2Translate = textToTranslate.text
+        var encodeTextSingle = encodeText()
+        var toTranslate = [encodeText]()
+        encodeTextSingle.text = text2Translate!
+        toTranslate.append(encodeTextSingle)
         
-        let url = URL(string: sampleLangAddress)
-        print(url!, azureKey)
-        //X-MT-ClientKey: 18358F19A2E74F80-A7C5BE039C8E614D
-        
+        let jsonToTranslate = try? jsonEncoder.encode(toTranslate)
+        let url = URL(string: apiURL)
         var request = URLRequest(url: url!)
-        
+
         request.httpMethod = "POST"
-        request.addValue(azureKey, forHTTPHeaderField: "X-MT-ClientKey")
+        request.addValue(azureKey, forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
         request.addValue(contentType, forHTTPHeaderField: "Content-Type")
         request.addValue(traceID, forHTTPHeaderField: "X-ClientTraceID")
         request.addValue(host, forHTTPHeaderField: "Host")
+        request.addValue(String(describing: jsonToTranslate?.count), forHTTPHeaderField: "Content-Length")
         request.httpBody = jsonToTranslate
         
-        //*****TO DO ADD THE TASK USING THE REQUEST AND RESUME TASK, ADD COMPLETION BLOCK AND THEN PARSE THE JSON
+        //print(String(data: jsonToTranslate!, encoding: .utf8)!)
+        let config = URLSessionConfiguration.default
+        let session =  URLSession(configuration: config)
+        
+        let task = session.dataTask(with: request) { (responseData, response, responseError) in
+            
+            print("*****this is the response from the request")
+            print("this is the response ", response!)
+            print("this is the response data ", responseData!)
+            
+            print(String(data: responseData!, encoding: .utf8)!)
+            if responseError != nil {
+                print("this is the error ", responseError!)
+            }
+            print("*****")
+            self.parseJson(jsonData: responseData!)
+        }
+        task.resume()
+//        DispatchQueue.main.async {
+//            self.translatedText.text = newTranslatedText
+//        }
+        
     }
     
+    
     //*****Class Methods
+    
+    func parseJson(jsonData: Data) {
+        
+        //*****TRANSLATION RETURNED DATA*****
+        struct ReturnedJson: Codable {
+            var translations: [TranslatedStrings]
+        }
+        struct TranslatedStrings: Codable {
+            var text: String
+            var to: String
+        }
+        
+        let jsonDecoder = JSONDecoder()
+        let langTranslations = try? jsonDecoder.decode(Array<ReturnedJson>.self, from: jsonData)
+        let numberOfTranslations = langTranslations!.count - 1
+        print(langTranslations!.count)
+        print("**********")
+        print("This is the translation -> ", langTranslations![0].translations[numberOfTranslations].text)
+        print("**********")
+        print("This is the language code -> ", langTranslations![0].translations[numberOfTranslations].to)
+        
+        DispatchQueue.main.async {
+            self.translatedText.text = langTranslations![0].translations[numberOfTranslations].text
+        }
+    }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -120,26 +176,21 @@ class Translation: UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
         let url1 = URL(string: sampleLangAddress)
         let jsonLangData = try! Data(contentsOf: url1!)
         
-        //*****used in the parsing of Json
+        //*****used in the parsing of request Json
         struct Translation: Codable {
             var translation: [String: LanguageDetails]
             
         }
-        
         struct LanguageDetails: Codable {
             var name: String
             var nativeName: String
             var dir: String
         }
-        
-        
+        //*****
         
         let jsonDecoder1 = JSONDecoder()
-        
         let languages = try? jsonDecoder1.decode(Translation.self, from: jsonLangData)
-        
         var eachLangInfo = AllLangDetails(code: " ", name: " ", nativeName: " ", dir: " ") //Use this instance to populate and then append to the array instance
-        
         
         //dump(languages)
         //print(languages!.translation.first?.key as Any)
@@ -168,7 +219,7 @@ class Translation: UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
         //arrayLangInfo.count
         //print(arrayLangInfo[60])
         arrayLangInfo.sort(by: {$0.code < $1.code})
-        print(arrayLangInfo)
+        //print(arrayLangInfo)
     }
     
     
