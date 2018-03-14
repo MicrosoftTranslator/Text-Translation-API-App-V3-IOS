0b236e9b6b0ed846d21095de3cf7050c28d8a697
KellyAltom <kellyaltom@hotmail.com>
2018-03-14 15:20:10 -0700

    minor

--- TranslatorV3Sample/Transliteration.swift
+++ TranslatorV3Sample/Transliteration.swift
@@ -24,10 +24,6 @@ class Transliteration: UIViewController, UIPickerViewDelegate, UIPickerViewDataS
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     
 }
