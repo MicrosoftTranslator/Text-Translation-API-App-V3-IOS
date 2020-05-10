d6d924762019e3d69d6a26d76603bd881e576902
Mark Fleming <markf@Marks-MacBook-Pro.local>
2020-05-10 11:52:45 -0400

    refactors JSON Structure and Key and host constants to separate files Add some error checking and feed back add log message to help people understand what data is being sent / received. add both native and english name to translation menu

---
+++ TranslatorV3Sample/KeysAndURLS.swift
@@ -0,0 +1,13 @@
+//
+//  KeysandURLS.swift
+//
+
+import Foundation
+
+ let azureKey = "75605c0edf924ef197302112e1bbdc2a"
+  let azureRegion = "eastus";
+  let contentType = "application/json"
+
+// X-ClientTraceId    *Optional request header*.  A client-generated GUID to uniquely identify the request.
+  let traceID = "A14C9DB9-0DED-48D7-8BBE-C517A1A8DBB0"
+  let host = "api.cognitive.microsofttranslator.com"
